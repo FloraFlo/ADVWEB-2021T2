06abb75555ddc38f5e2bedb11702e9a63466802e
8432 <8432@ait-sydney.net.au>
2021-06-18 11:04:06 +1000

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+//comment 
+$name = "Flora"; //variable dollar sign in front of it 
+$day = "Friday"; 
+
+$date = 26; //no quotes for numbers 
+
+$test ="Hello PHP"; 
+
+echo "Hello, ". $name . " Today is ".$day; //. is like +(concatenated), if you want to see space you need to add in in betweek quotes  
+
+echo "<h1>$date</h1>"; //output html without double quotes 
+
+echo '<p>'. $day . '</p>'; //output html with single quotes 
+?> 
+
+<div style="background-color:green;color:white;">
+    <?php echo $test ?> 
+</div>
